300836b5fd3dee6ba0e3d6af0e8873f6120b76ce
PravasTheDeveloper <info.pravas.cs@gmail.com>
2023-10-03 19:54:00 +0600

    Setup for creating the website

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <div className='w-screen h-screen bg-slate-800'>
+
+      </div>
+    </>
+  )
+}
